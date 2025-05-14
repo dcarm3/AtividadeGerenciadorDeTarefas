69913a7bcda317920f8ea37453c44dd1c5379cee
dcarm3 <dcardosom3@gmail.com>
2025-05-14 19:13:19 -0300

    Tarefa Repository criado

--- src/main/java/com/example/AtividadeGerenciadorDeTarefas/model/Tarefa.java
+++ src/main/java/com/example/AtividadeGerenciadorDeTarefas/model/Tarefa.java
@@ -14,7 +14,7 @@ public class Tarefa {
     private String title;
     private String description;
     private String status;
-    
+
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
