13750bc2d1a9c8551c4bed58f1db77beca2744f1
dcarm3 <dcardosom3@gmail.com>
2025-05-13 21:42:28 -0300

    Projeto criado

---
+++ src/main/java/com/example/AtividadeGerenciadorDeTarefas/AtividadeGerenciadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.example.AtividadeGerenciadorDeTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeGerenciadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeGerenciadorDeTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AtividadeGerenciadorDeTarefas/AtividadeGerenciadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AtividadeGerenciadorDeTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeGerenciadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
